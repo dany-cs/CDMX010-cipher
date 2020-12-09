f11f17b08dda85c6bacd4cc732d3257d7ef314ba
dany correo <danycorreos96@gmail.com>
2020-12-08 21:27:06 -0600

    prueba

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,69 @@
-const cipher = {
-  // ...
-};
+/**
+ * @param {numero} offset 
+ * @param {string} mensaje
+ * esta funcion recibe un numero que servira para el cifrado y la palanta a cofrar
+ * regresa una palabra cifrada  
+ */
+function encode(offset,mensaje){
+const alfabeto= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","w","X","Y","Z"];
+/**
+ * arreglos cuentan con la venaja de que te van a regresar el valor 
+ */
+let nuevoAlfabeto = [];//arreglo vacio
+//console.log(alfabet
+//console.log(offset)
+//console.log(mensaje)
+let i;
+let nuevomensaje;
+for(i=offset; i<alfabeto.length; i=i+1){
+  nuevoAlfabeto.push(alfabeto[i])
+}
 
-export default cipher;
+for(i=0; i<offset; i=i+1){
+  nuevoAlfabeto.push(alfabeto[i])
+}
+
+/**for (inicio,condicion,incremento)
+ * inicia en cero
+ * mientras i sea menor que la longitud del mensaje 
+ * sumara a i (incrementara mientras entre en la longitud del mensaje)
+ */
+
+for(i=0; i<mensaje.length; i=i+1){
+  console.log(mensaje[i])
+  nuevoAlfabeto.push(alfabeto[i])
+}
+console.log(mensaje);
+console.log(nuevomensaje)
+
+}
+
+
+
+function principal() {
+  console.log("")
+  encode(3,"HOLA");
+}
+
+
+principal();
+
+//funcion Decipher
+
+  //tratando de invertir la function cipher
+function decipher(text){
+  //para guardar las letras de la letra a descifrar
+  var textDecipher = "";
+  //
+    for(var i = 0; i<text.length; i++){
+      var textChar = (String.fromCharCode(text(i)) + 33)%26;
+      textDecipher = textDecipher + textChar;
+      var newText = newText.charCodeAt(textDecipher);
+    }
+    return newText;
+c
+  }
+  export default cipher;
+  //formula (x-65+n)%26+65
+
+  //return resultado//
--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
+/*
 import cipher from './cipher.js';
-
+//Aqui se manipula el DOM
 console.log(cipher);
+*/
+
+//La siguiente linea accede al DOM
+/**let nodo=document.querySelector(".cesitar"); //Esta no es la unica forma de acceder al dom
+//La siguiente linea es una variable con el texto a insertar en la linea 12
+let nombre="Holaaaaa Chavas me llamo DANNY";
+//La siguiente linea inserta HTML al DOM
+nodo.innerHTML=`<h1>${nombre}</h1>`;
+alert("hola");*/
+function inicialb() {
+    document.getElementById("ctexto").style.display = "block";
+    document.getElementById("inicial").style.display = "none";
+
+}
+
+function textoa() {
+    document.getElementById("msfin").style.display="block";
+    document.getElementById("ctexto").style.display="none";
+    
+}
+
+function principal() {
+    document.getElementById("inicial").style.display="block";
+    document.getElementById("msfin").style,display="none";
+    
+}
+//function clickfunction(){
+   // let cajauno=document.getElementById("texcifrar");
+    //let offset=document.getElementById(offset);//
+    //console.log=(offset.value);
+    //alert (cajauno.value);
+    //let cajatres= cajauno.value
+
+
